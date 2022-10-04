b31d49b9917b1992cee8d787082f2a264e503794
martin-sainos-demian <martin.sainos.demian@gmail.com>
2022-10-04 12:46:49 -0500

    update

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express = require("express");
 
+var app = express();
+
 const PORT = process.env.PORT || 420;
 
-const app = express();
+app.all("/", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+app.get("/", (req, res, next) => {
+  res.json({ 
+    data : "never gonna give you up",
+    message: "lel" });
+});
+app.post("/", function (req, res, next) {
+  // Handle the post for this route
+});
 
-app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
-  });
-  
-  app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
-  });
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
