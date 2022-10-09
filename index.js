3caa793af07a9d4f25feb14130d9188c6832105a
martin-sainos-demian <111402496+martin-sainos-demian@users.noreply.github.com>
2022-10-09 17:40:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.all("/", function (req, res, next) {
 //end of global
 
 //database
-const CyclicDb = require("cyclic-dynamodb")
+const CyclicDB = require("cyclic-dynamodb")
 const db = CyclicDB("bewildered-moth-cardiganCyclicDB")
 
  //users
