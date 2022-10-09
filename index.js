bf9680fddb89c278b5d34e7fde78808a255e8146
martin-sainos-demian <111402496+martin-sainos-demian@users.noreply.github.com>
2022-10-09 18:12:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -87,7 +87,8 @@ app.get("/register", (req, res, next) => {
     while(getNameById(newId)!=-1){
       newId= Math.random()*10000
     }
-    let newReg = users.set(newId.toString(), {
+    newId=newId.toString()
+    let newReg = users.set(newId, {
       usu_name: en(usu),
       usu_mpassword: en(pass)
     })
