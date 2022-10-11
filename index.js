1c20c905cbad30c2aa12ca0fd2d136416f8ef163
martin-sainos-demian <martin.sainos.demian@gmail.com>
2022-10-11 12:34:53 -0500

    test

--- index.js
+++ index.js
@@ -45,6 +45,7 @@ const logEverything= async function(){
   for(var reg in usersFull){
     console.log("reg "+JSON.stringify(usersFull[reg])+" : "+usersFull[reg].key.toString()+JSON.stringify(users.get(usersFull[reg].key.toString())))
   }
+  console.log("3 "+JSON.stringify(users.get("3")))
 }
 const delEverything= async function(){
   console.log("im die thank you foreva")
