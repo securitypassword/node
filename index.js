7123b399f2e708eb8362773d866dd2dc2a03a512
martin-sainos-demian <martin.sainos.demian@gmail.com>
2022-10-12 11:05:12 -0500

    register but dont chek if exists

--- index.js
+++ index.js
@@ -65,6 +65,7 @@ const usersEmptyId=async function(){
   var newId=Math.random*10000
   newId=Math.floor(newId)
   var empty=await users.get(newId.toString())
+  console.log(JSON.stringify(empty))
   while(JSON.stringify(empty)!=null){
     newId=Math.random*10000
     newId=Math.floor(newId)
