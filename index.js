d66ceb30d4ccd35dfcf131bff67d83f9ece967fb
martin-sainos-demian <martin.sainos.demian@gmail.com>
2022-10-18 15:51:37 -0500

    autodel test

--- index.js
+++ index.js
@@ -154,7 +154,7 @@ app.get("/register", async (req, res, next) => {
 
 const addAutoDel=async function(usu_id){
   var id=usu_id.substring(1,usu_id.length-1)
-  var user=await users.get()
+  var user=await users.get(id)
   console.log(id)
   if(user.props.usu_autodel=="true"){
     var count=users.props.usu_autodel_count
