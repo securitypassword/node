a3ae81af0c375870edbf961c72ebca91435cb87a
martin-sainos-demian <martin.sainos.demian@gmail.com>
2022-10-18 20:37:45 -0500

    fix bin

--- index.js
+++ index.js
@@ -273,7 +273,7 @@ const binFromUser= async function(usu_id){
   var resp={}
   for(var r in regsUser){
     console.log("lol "+JSON.stringify(regsUser[r]))
-    if(regsUser[r].props.reg_in_bin==true){
+    if(regsUser[r].props.reg_in_bin){
       resp[r.toString()]={}
       var name=de(regsUser[r].props.reg_name)
       resp[r.toString()]["reg_name"]=en(name)
@@ -321,9 +321,9 @@ app.get("/getPaperBin", async (req, res, next) => {
 const deleteRegister= async function(reg_id){
   var reg=await regs.get(reg_id)
   console.log(JSON.stringify(reg))
-  if(reg.props.reg_in_bin==false){
+  if(!reg.props.reg_in_bin){
     regs.set(reg_id,{reg_in_bin:true})
-  }else if(reg.props.reg_in_bin==true){
+  }else{
     await regs.delete(reg_id)
     console.log("delete reg "+reg_id)
   }
