ad633d6985cfcd8242ac068ba4335cd39a47576b
martin-sainos-demian <martin.sainos.demian@gmail.com>
2022-11-06 14:06:55 -0600

    free3

--- js/free.js
+++ js/free.js
@@ -1,3 +1,5 @@
+var sec = require("./sec")
+
 var run = function(app){
     app.get("/generate", (req, res, next) => {
     var allowed = "";
@@ -31,7 +33,7 @@ var run = function(app){
     for (var i = 0; i < len; i++) {
       pass += allowed.charAt(Math.floor(Math.random() * allowed.length));
     }
-    var passEn = en(pass);
+    var passEn = sec.en(pass);
     res.json({
       data: passEn,
       msg: "random password",
---
+++ js/sec.js
@@ -0,0 +1,16 @@
+var de = function(text) {
+    return CryptoJS.DES.decrypt(
+      text.replace(/-/g, "+").replace(/_/g, "/"),
+      key
+    ).toString(CryptoJS.enc.Utf8);
+  };
+  var en = function (text) {
+    return CryptoJS.DES.encrypt(text, key)
+      .toString()
+      .replace(/\+/g, "-")
+      .replace(/\//g, "_");
+  };
+
+  
+module.exports.de = de;
+module.exports.en = en;
