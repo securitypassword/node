707c3d42fbd25832bfb974b42c2e94ba8fdb4f55
martin-sainos-demian <martin.sainos.demian@gmail.com>
2022-11-07 06:16:17 -0600

    db

--- js/sql.js
+++ js/sql.js
@@ -1,58 +1,38 @@
-var Connection = require('tedious').Connection;  
-var config = {  
-    server: 'sql9.freemysqlhosting.net',  //update me
-    authentication: {
-        type: 'default',
-        options: {
-            userName: 'sql9540023', //update me
-            password: 'Y2TNk21TwE'  //update me
-        }
+const {Connection, Request} = require("tedious");
+const executeSQL = (sql, callback) => {
+  let connection = new Connection({
+    "authentication": {
+      "options": {
+        "userName": "sql9540023",
+        "password": "Y2TNk21TwE"
+      },
+      "type": "default"
     },
-    options: {
-        // If you are on Microsoft Azure, you need encryption:
-        encrypt: true,
-        database: 'sql9540023'  //update me
+    "server": "sql9.freemysqlhosting.net",
+    "options": {
+      "validateBulkLoadParameters": false,
+      "rowCollectionOnRequestCompletion": true,
+      "database": "DATABASE",
+      "encrypt": true
     }
-};
-var connection = new Connection(config);  
-connection.on('connect', function(err) {  
-    // If no error, then good to proceed.  
-    console.log("Connected");  
-    executeStatement();  
-});  
-
-connection.connect();
-
-var Request = require('tedious').Request;  
-var TYPES = require('tedious').TYPES;  
-
-function executeStatement() {  
-    request = new Request("SELECT SELECT * FROM `rol`;", function(err) {  
-    if (err) {  
-        console.log(err);}  
-    });  
-    var result = "";  
-    request.on('row', function(columns) {  
-        columns.forEach(function(column) {  
-          if (column.value === null) {  
-            console.log('NULL');  
-          } else {  
-            result+= column.value + " ";  
-          }  
-        });  
-        console.log(result);  
-        result ="";  
-    });  
-
-    request.on('done', function(rowCount, more) {  
-    console.log(rowCount + ' rows returned');  
-    });  
-    
-    // Close the connection after the final event emitted by the request, after the callback passes
-    request.on("requestCompleted", function (rowCount, more) {
-        connection.close();
+  });
+  connection.connect((err) => {
+    if (err)
+      return callback(err, null);
+    const request = new Request(sql, (err, rowCount, rows) => {
+      connection.close();
+      if (err)
+        return callback(err, null);
+      callback(null, {rowCount, rows});
     });
-    connection.execSql(request);  
-}  
-
+    connection.execSql(request);
+  });
+};
+const executeStatement = function(){
+    executeSQL("SELECT * FROM users", (err, data) => {
+    if (err)
+        console.error(err);
+    console.log(data.rowCount);
+    });
+}
 module.exports.executeStatement = executeStatement;
