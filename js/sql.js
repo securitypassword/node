1cbffd5ea7b8e70df7963926a59a150d8fecbeb0
martin-sainos-demian <martin.sainos.demian@gmail.com>
2022-11-07 07:40:22 -0600

    mysql

--- js/sql.js
+++ js/sql.js
@@ -1,41 +1,17 @@
-const {Connection, Request} = require("tedious");
-const executeSQL = (sql, callback) => {
-  let connection = new Connection({
-    "authentication": {
-      "options": {
-        "userName": "sql9540023",
-        "password": "Y2TNk21TwE"
-      },
-      "type": "default"
-    },
-    "server": "sql9.freemysqlhosting.net",
-    "options": {
-      "validateBulkLoadParameters": false,
-      "rowCollectionOnRequestCompletion": true,
-      "database": "sql9540023",
-      "port" : 3306,
-      "encrypt": true,
-      "trustedConnection": true 
-    }
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "yourusername",
+    password: "yourpassword"
   });
-  connection.connect((err) => {
-    if (err)
-      return callback(err, null);
-    const request = new Request(sql, (err, rowCount, rows) => {
-      connection.close();
-      if (err)
-        return callback(err, null);
-      callback(null, {rowCount, rows});
-    });
-    connection.execSql(request);
-  });
-};
+  
+
+
 const executeStatement = function(){
-    executeSQL("SELECT * FROM rol", (err, data) => {
-    if (err)
-        console.error(err);
-        console.log("sql uwu")
-    console.log(data);
-    });
+    con.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+      });
 }
 module.exports.executeStatement = executeStatement;
