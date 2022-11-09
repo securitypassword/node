75b5d2c4fb55c35ee8fa28338991bfc61152ccc1
martin-sainos-demian <martin.sainos.demian@gmail.com>
2022-11-09 01:00:25 -0600

    reg user

--- js/db/user.js
+++ js/db/user.js
@@ -9,13 +9,13 @@ const userExists = async function(name){
     let res = await sql.sql(command)
     console.log("user exists")
     console.log(res)    
-    return res.length>0
+    return res==void(0)
 }
 
 const usersEmptyId = async function(name){
     let newId=1
     let empty=""
-    while(empty.length>-0){
+    while(res!=void(0)){
       newId=Math.random*1000000
       newId=Math.floor(newId)
       let command="SELECT * FROM `usuario` WHERE `usu_nombre`=`"+newId+"`"
